7d705701d726412998dceae646b5c6f5007467ab
njugunamwangi <njugunamwangi.n@gmail.com>
2024-03-13 15:37:20 +0300

    Admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Ndachi',
+            'email' => 'info@ndachi.dev',
+            'password' => bcrypt('Password')
+        ]);
+    }
+}
