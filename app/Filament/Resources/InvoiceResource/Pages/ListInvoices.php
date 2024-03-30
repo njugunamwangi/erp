d2ed4a056420e95f39732a493c262cc0fe85a05f
njugunamwangi <njugunamwangi.n@gmail.com>
2024-03-31 00:00:20 +0300

    Invoice tabs

--- app/Filament/Resources/InvoiceResource/Pages/ListInvoices.php
+++ app/Filament/Resources/InvoiceResource/Pages/ListInvoices.php
@@ -4,7 +4,10 @@ namespace App\Filament\Resources\InvoiceResource\Pages;
 
 use App\Filament\Resources\InvoiceResource;
 use App\Filament\Resources\InvoiceResource\Widgets\InvoiceStatsOverview;
+use App\InvoiceStatus;
+use App\Models\Invoice;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListInvoices extends ListRecords
@@ -22,5 +25,23 @@ class ListInvoices extends ListRecords
         return [
             InvoiceStatsOverview::class,
         ];
+    }
+
+    public function getTabs(): array
+    {
+        $tabs = [];
+
+        $tabs['all'] = Tab::make('All Invoices')
+            ->badge(Invoice::count());
+
+        $tabs['paid'] = Tab::make('Paid')
+            ->badge(Invoice::where('status', '=', InvoiceStatus::Paid)->count())
+            ->modifyQueryUsing(fn($query) => $query->where('status', '=', InvoiceStatus::Paid));
+
+        $tabs['unpaid'] = Tab::make('Unpaid')
+            ->badge(Invoice::where('status', '=', InvoiceStatus::Unpaid)->count())
+            ->modifyQueryUsing(fn($query) => $query->where('status', '=', InvoiceStatus::Unpaid));
+
+        return $tabs;
     }
 }
