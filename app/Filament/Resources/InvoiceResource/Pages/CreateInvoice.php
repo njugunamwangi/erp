6569acc3a47bafd70c0956db7231863ec1232319
njugunamwangi <njugunamwangi.n@gmail.com>
2024-04-03 14:11:38 +0300

    Refactor

--- app/Filament/Resources/InvoiceResource/Pages/CreateInvoice.php
+++ app/Filament/Resources/InvoiceResource/Pages/CreateInvoice.php
@@ -21,7 +21,6 @@ class CreateInvoice extends CreateRecord
         $data['serial_number'] = (Invoice::max('serial_number') ?? 0) + 1;
         $data['serial'] = $data['series'] . '-' . str_pad($data['serial_number'], 5, '0', STR_PAD_LEFT);
 
-        // dd($data);
         return $data;
     }
 }
