9f9eb344b4c7e1e42e8aa8023d02c25fd5557fbe
njugunamwangi <njugunamwangi.n@gmail.com>
2024-04-04 13:50:44 +0300

    Staff Panel

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\StaffPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
 ];
