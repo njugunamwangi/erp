5b70a66b2ae242e5099e455e5b549558cecaf992
njugunamwangi <njugunamwangi.n@gmail.com>
2024-04-09 13:04:07 +0300

    User generate invoice

--- app/Filament/Resources/UserResource/Pages/ViewUser.php
+++ app/Filament/Resources/UserResource/Pages/ViewUser.php
@@ -2,10 +2,14 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
+use App\Filament\Resources\InvoiceResource;
 use App\Filament\Resources\QuoteResource;
 use App\Filament\Resources\UserResource;
 use App\Filament\Resources\UserResource\Widgets\StaffTasksWidget;
 use App\Filament\Resources\UserResource\Widgets\TasksWidget;
+use App\InvoiceSeries;
+use App\InvoiceStatus;
+use App\Models\Invoice;
 use App\Models\Quote;
 use App\Models\Role;
 use App\Models\User;
@@ -137,7 +141,7 @@ class ViewUser extends ViewRecord
                                 ->title('Quote generated')
                                 ->body(auth()->user()->name.' generated a quote for ' . $record->name)
                                 ->icon('heroicon-o-check-badge')
-                                ->success()
+                                ->info()
                                 ->actions([
                                     ActionsAction::make('View')
                                         ->url(QuoteResource::getUrl('view', ['record' => $quote->id]))
@@ -150,6 +154,112 @@ class ViewUser extends ViewRecord
                     ->label('Generate Invoice')
                     ->color('primary')
                     ->icon('heroicon-o-clipboard-document-check')
+                    ->modalSubmitActionLabel('Generate Invoice')
+                    ->form([
+                        Grid::make(2)
+                            ->schema([
+                                Select::make('status')
+                                    ->enum(InvoiceStatus::class)
+                                    ->options(InvoiceStatus::class)
+                                    ->searchable()
+                                    ->required()
+                                    ->default(InvoiceStatus::Unpaid->name),
+                                Select::make('series')
+                                    ->required()
+                                    ->enum(InvoiceSeries::class)
+                                    ->options(InvoiceSeries::class)
+                                    ->searchable()
+                                    ->preload()
+                                    ->default(InvoiceSeries::IN2INV->name),
+                            ]),
+                            Fieldset::make('Invoice Summary')
+                                ->schema([
+                                    Section::make()
+                                        ->schema([
+                                            Repeater::make('items')
+                                                ->columns(4)
+                                                ->live()
+                                                ->schema([
+                                                    TextInput::make('quantity')
+                                                        ->numeric()
+                                                        ->required()
+                                                        ->live()
+                                                        ->default(1),
+                                                    TextInput::make('description')
+                                                        ->required()
+                                                        ->placeholder('Aerial Spraying'),
+                                                    TextInput::make('unit_price')
+                                                        ->required()
+                                                        ->live()
+                                                        ->numeric()
+                                                        ->default(1000),
+                                                    Placeholder::make('sum')
+                                                        ->label('Sub Total')
+                                                        ->live()
+                                                        ->content(function (Get $get) {
+                                                            return 'Kes '.number_format($get('quantity') * $get('unit_price'), 2, '.', ',');
+                                                        }),
+                                                ])
+                                                ->afterStateUpdated(function (Get $get, Set $set) {
+                                                    self::updateTotals($get, $set);
+                                                })
+                                                ->addActionLabel('Add Item')
+                                                ->columnSpanFull(),
+                                        ]),
+                                    Section::make()
+                                        ->schema([
+                                            TextInput::make('subtotal')
+                                                ->numeric()
+                                                ->readOnly()
+                                                ->prefix('Kes')
+                                                ->afterStateHydrated(function (Get $get, Set $set) {
+                                                    self::updateTotals($get, $set);
+                                                }),
+                                            TextInput::make('taxes')
+                                                ->suffix('%')
+                                                ->required()
+                                                ->numeric()
+                                                ->default(16)
+                                                ->live(true)
+                                                ->afterStateUpdated(function (Get $get, Set $set) {
+                                                    self::updateTotals($get, $set);
+                                                }),
+                                            TextInput::make('total')
+                                                ->numeric()
+                                                ->readOnly()
+                                                ->prefix('Kes'),
+                                        ]),
+                                ])
+                    ])
+                    ->action(function(array $data, $record) {
+                        $invoice = $record->invoices()->create([
+                            'user_id' => $record->id,
+                            'status' => $data['status'],
+                            'subtotal' => $data['subtotal'],
+                            'taxes' => $data['taxes'],
+                            'total' => $data['total'],
+                            'items' => $data['items'],
+                            'series' => $data['series'],
+                            'serial_number' => $serial_number = Invoice::max('serial_number') + 1,
+                            'serial' => $data['series'].'-'.str_pad($serial_number, 5, '0', STR_PAD_LEFT),
+                        ]);
+
+                        $recipients = User::role(Role::ADMIN)->get();
+
+                        foreach ($recipients as $recipient) {
+                            Notification::make()
+                                ->title('Invoice generated')
+                                ->body(auth()->user()->name.' generated a quote for ' . $record->name)
+                                ->icon('heroicon-o-check-badge')
+                                ->success()
+                                ->actions([
+                                    ActionsAction::make('View')
+                                        ->url(InvoiceResource::getUrl('view', ['record' => $invoice->id]))
+                                        ->markAsRead(),
+                                ])
+                                ->sendToDatabase($recipient);
+                            }
+                    })
             ])
         ];
     }
