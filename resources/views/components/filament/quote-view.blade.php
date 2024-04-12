369d23b2156dbb989f9296d84eb9183f05a993bc
njugunamwangi <njugunamwangi.n@gmail.com>
2024-04-12 14:28:13 +0300

    Refactor

--- resources/views/components/filament/quote-view.blade.php
+++ resources/views/components/filament/quote-view.blade.php
@@ -1,3 +1,5 @@
-<iframe src="{{ URL::signedRoute('quote.view', [$record->id, 'preview' => true]) }}" style="min-height: 100svh;" class="w-full">
+<iframe src="{{ URL::signedRoute('quote.view', [$record->id, 'preview' => true]) }}"
+    style="min-height: 100svh;"
+    class="w-full">
 
 </iframe>
