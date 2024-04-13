2f908910bc338222d777eba42b625929f82b10a2
njugunamwangi <njugunamwangi.n@gmail.com>
2024-04-13 13:04:44 +0300

    Staff notification

--- app/Filament/Resources/TaskResource/Pages/CreateTask.php
+++ app/Filament/Resources/TaskResource/Pages/CreateTask.php
@@ -3,6 +3,9 @@
 namespace App\Filament\Resources\TaskResource\Pages;
 
 use App\Filament\Resources\TaskResource;
+use App\Filament\Staff\Resources\TaskResource as ResourcesTaskResource;
+use Filament\Notifications\Actions\Action;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateTask extends CreateRecord
@@ -14,5 +17,23 @@ class CreateTask extends CreateRecord
         $data['assigned_by'] = auth()->id();
 
         return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        $task = $this->getRecord();
+
+        Notification::make()
+            ->warning()
+            ->icon('heroicon-o-bolt')
+            ->title('Task assigned')
+            ->body('Attend to task #' . $task->id . ' for ' . $task->assignedFor->name)
+            ->actions([
+                Action::make('view')
+                    ->markAsRead()
+                    ->url(ResourcesTaskResource::getUrl('view', ['record' => $task->id]))
+                    ->color('warning')
+            ])
+            ->sendToDatabase($task->assignedTo);
     }
 }
