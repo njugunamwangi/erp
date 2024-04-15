71f6a641aa959ddd895041b94ee827e4188754f5
njugunamwangi <njugunamwangi.n@gmail.com>
2024-04-15 18:47:17 +0300

    Material enum

---
+++ app/Enums/Material.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+enum Material: string
+{
+    case Water = 'Water';
+    case Cement = 'Cement';
+    case Sand = 'Sand';
+    case PVC = 'PVC';
+    case Paint = 'Paint';
+    case Brushes = 'Brushes';
+}
