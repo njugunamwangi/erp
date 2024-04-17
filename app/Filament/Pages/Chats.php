1e94415901d653dd65699941dafbd304c57f0bdd
njugunamwangi <njugunamwangi.n@gmail.com>
2024-04-18 00:50:55 +0300

    Admin chats layout

---
+++ app/Filament/Pages/Chats.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class Chats extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-chat-bubble-left-right';
+
+    protected static string $view = 'filament.pages.chats';
+}
---
+++ resources/views/filament/pages/chats.blade.php
@@ -0,0 +1,21 @@
+<x-filament-panels::page class="">
+    <x-filament::section class="h-[300px] w-1/4">
+        <x-slot name="heading">
+            Messages
+        </x-slot>
+
+        Content
+    </x-filament::section>
+
+    <x-filament::section class="h-[300px] w-3/4">
+        <x-slot name="heading">
+            Message Body
+        </x-slot>
+
+        <x-slot name="description">
+            This is all the information we hold about the user.
+        </x-slot>
+
+        {{-- Content --}}
+    </x-filament::section>
+</x-filament-panels::page>
