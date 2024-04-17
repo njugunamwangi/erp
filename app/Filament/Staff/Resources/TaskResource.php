2b7463cf6164b69cdd2b6e208a08fe7399725796
njugunamwangi <njugunamwangi.n@gmail.com>
2024-04-17 10:29:31 +0300

    Staff Expenses

--- app/Filament/Staff/Resources/TaskResource.php
+++ app/Filament/Staff/Resources/TaskResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Staff\Resources;
 
 use App\Filament\Staff\Resources\TaskResource\Pages;
 use App\Filament\Staff\Resources\TaskResource\RelationManagers;
+use App\Models\Expense;
 use App\Models\Role;
 use App\Models\Task;
 use App\Models\User;
@@ -16,6 +17,7 @@ use Filament\Infolists\Infolist;
 use Filament\Notifications\Actions\Action as ActionsAction;
 use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
+use Filament\Support\Enums\MaxWidth;
 use Filament\Tables;
 use Filament\Tables\Actions\Action as TablesActionsAction;
 use Filament\Tables\Actions\BulkAction;
@@ -109,7 +111,62 @@ class TaskResource extends Resource
                                 ])
                                 ->sendToDatabase($recipient);
                         }
-                    })
+                    }),
+                    TablesActionsAction::make('expenses')
+                        ->icon('heroicon-o-arrow-trending-up')
+                        ->color('danger')
+                        ->modalWidth(MaxWidth::FiveExtraLarge)
+                        ->stickyModalFooter()
+                        ->stickyModalHeader()
+                        ->modalSubmitActionLabel('Save')
+                        ->fillForm(fn (Task $record): array => [
+                            'accommodation' => $record->expense?->accommodation,
+                            'subsistence' => $record->expense?->subsistence,
+                            'fuel' => $record->expense?->fuel,
+                            'labor' => $record->expense?->labor,
+                            'material' => $record->expense?->material,
+                            'misc' => $record->expense?->misc,
+                        ])
+                        ->form(Expense::getForm())
+                        ->action(function(Task $task, array $data) {
+                            if($task->expense) {
+                                $task->expense()->update([
+                                    'accommodation' => $data['accommodation'],
+                                    'subsistence' => $data['subsistence'],
+                                    'fuel' => $data['fuel'],
+                                    'labor' => $data['labor'],
+                                    'material' => $data['material'],
+                                    'misc' => $data['misc'],
+                                ]);
+                            } else {
+                                $task->expense()->create([
+                                    'task_id' => $task->id,
+                                    'accommodation' => $data['accommodation'],
+                                    'subsistence' => $data['subsistence'],
+                                    'fuel' => $data['fuel'],
+                                    'labor' => $data['labor'],
+                                    'material' => $data['material'],
+                                    'misc' => $data['misc'],
+                                ]);
+                            }
+                        })
+                        ->after(function(Task $record) {
+                            if($record->expense) {
+                                Notification::make()
+                                    ->title('Expense updated')
+                                    ->info()
+                                    ->icon('heroicon-o-check')
+                                    ->body('Task expenses have been updated successfully')
+                                    ->send();
+                            } else {
+                                Notification::make()
+                                    ->title('Expense created')
+                                    ->success()
+                                    ->icon('heroicon-o-check')
+                                    ->body('Task expenses have been created successfully')
+                                    ->send();
+                            }
+                        })
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
--- app/Filament/Staff/Resources/TaskResource/Pages/ViewTask.php
+++ app/Filament/Staff/Resources/TaskResource/Pages/ViewTask.php
@@ -3,8 +3,13 @@
 namespace App\Filament\Staff\Resources\TaskResource\Pages;
 
 use App\Filament\Staff\Resources\TaskResource;
+use App\Models\Expense;
+use App\Models\Task;
 use Filament\Actions;
+use Filament\Actions\Action;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Support\Enums\MaxWidth;
 
 class ViewTask extends ViewRecord
 {
@@ -13,7 +18,63 @@ class ViewTask extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('expenses')
+                ->icon('heroicon-o-arrow-trending-up')
+                ->color('danger')
+                ->modalWidth(MaxWidth::FiveExtraLarge)
+                ->stickyModalFooter()
+                ->stickyModalHeader()
+                ->modalSubmitActionLabel('Save')
+                ->fillForm(fn (Task $record): array => [
+                    'accommodation' => $record->expense?->accommodation,
+                    'subsistence' => $record->expense?->subsistence,
+                    'fuel' => $record->expense?->fuel,
+                    'labor' => $record->expense?->labor,
+                    'material' => $record->expense?->material,
+                    'misc' => $record->expense?->misc,
+                ])
+                ->form(Expense::getForm())
+                ->action(function(array $data) {
+                    $task = $this->getRecord();
 
+                    if($task->expense) {
+                        $task->expense()->update([
+                            'accommodation' => $data['accommodation'],
+                            'subsistence' => $data['subsistence'],
+                            'fuel' => $data['fuel'],
+                            'labor' => $data['labor'],
+                            'material' => $data['material'],
+                            'misc' => $data['misc'],
+                        ]);
+                    } else {
+                        $task->expense()->create([
+                            'task_id' => $task->id,
+                            'accommodation' => $data['accommodation'],
+                            'subsistence' => $data['subsistence'],
+                            'fuel' => $data['fuel'],
+                            'labor' => $data['labor'],
+                            'material' => $data['material'],
+                            'misc' => $data['misc'],
+                        ]);
+                    }
+                })
+                ->after(function(Task $record) {
+                    if($record->expense) {
+                        Notification::make()
+                            ->title('Expense updated')
+                            ->info()
+                            ->icon('heroicon-o-check')
+                            ->body('Task expenses have been updated successfully')
+                            ->send();
+                    } else {
+                        Notification::make()
+                            ->title('Expense created')
+                            ->success()
+                            ->icon('heroicon-o-check')
+                            ->body('Task expenses have been created successfully')
+                            ->send();
+                    }
+                })
         ];
     }
 }
