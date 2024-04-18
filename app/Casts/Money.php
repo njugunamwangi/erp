6583a06aaf8c46bdd752282e882d25a93fca8067
njugunamwangi <njugunamwangi.n@gmail.com>
2024-04-18 18:17:30 +0300

    Money casts

---
+++ app/Casts/Money.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
+
+class Money implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function get(Model $model, string $key, mixed $value, array $attributes): mixed
+    {
+        return \Brick\Money\Money::ofMinor($value, $attributes['currency']);
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function set(Model $model, string $key, mixed $value, array $attributes): mixed
+    {
+        if (! $value instanceof \Brick\Money\Money) {
+            return $value;
+        }
+
+        return $value->getMinorAmount()->toInt();
+    }
+}
