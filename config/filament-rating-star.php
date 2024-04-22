5588d35275164c34683178653e02e149ff9846d0
njugunamwangi <njugunamwangi.n@gmail.com>
2024-04-22 18:23:36 +0300

    Rating star config

---
+++ config/filament-rating-star.php
@@ -0,0 +1,13 @@
+<?php
+
+// config for IbrahimBougaoua/FilamentRatingStar
+return [
+    'stars' => [
+        'star1' => '1',
+        'star2' => '2',
+        'star3' => '3',
+        'star4' => '4',
+        'star5' => '5',
+    ],
+    'isRTL' => false,
+];
