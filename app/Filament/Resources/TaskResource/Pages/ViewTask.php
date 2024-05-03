cd52d2b0c50e5c49fc9dd2777d9abedc26f7a21d
njugunamwangi <njugunamwangi.n@gmail.com>
2024-05-03 11:25:44 +0300

    Generate Task quote

--- app/Filament/Resources/TaskResource/Pages/ViewTask.php
+++ app/Filament/Resources/TaskResource/Pages/ViewTask.php
@@ -2,12 +2,29 @@
 
 namespace App\Filament\Resources\TaskResource\Pages;
 
+use App\Enums\QuoteSeries;
+use App\Filament\Resources\QuoteResource;
 use App\Filament\Resources\TaskResource;
+use App\Models\Currency;
 use App\Models\Expense;
+use App\Models\Quote;
+use App\Models\Role;
 use App\Models\Task;
+use App\Models\User;
 use Filament\Actions;
 use Filament\Actions\Action;
 use Filament\Actions\ActionGroup;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Filament\Notifications\Actions\Action as ActionsAction;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ViewRecord;
 use Filament\Support\Enums\MaxWidth;
@@ -78,7 +95,149 @@ class ViewTask extends ViewRecord
                                 ->send();
                         }
                     }),
+                Action::make('quote')
+                    ->icon('heroicon-o-banknotes')
+                    ->color('warning')
+                    ->label('Generate Quote')
+                    ->modalDescription(fn($record) => 'Quote for task #'.$record->id)
+                    ->modalSubmitActionLabel('Generate Quote')
+                    ->visible(fn($record) => !$record->quote)
+                    ->form([
+                        Grid::make(2)
+                            ->schema([
+                                Select::make('series')
+                                    ->required()
+                                    ->enum(QuoteSeries::class)
+                                    ->options(QuoteSeries::class)
+                                    ->searchable()
+                                    ->preload()
+                                    ->default(QuoteSeries::IN2QUT->name),
+                                Select::make('currency_id')
+                                    ->label('Currency')
+                                    ->optionsLimit(40)
+                                    ->searchable()
+                                    ->createOptionForm(Currency::getForm())
+                                    ->live()
+                                    ->preload()
+                                    ->getSearchResultsUsing(fn (string $search): array => Currency::whereAny([
+                                        'name', 'abbr', 'symbol', 'code'], 'like', "%{$search}%")->limit(50)->pluck('abbr', 'id')->toArray())
+                                    ->getOptionLabelUsing(fn ($value): ?string => Currency::find($value)?->abbr)
+                                    ->loadingMessage('Loading currencies...')
+                                    ->searchPrompt('Search currencies by their symbol, abbreviation or country')
+                                    ->required(),
+                            ]),
+                        Fieldset::make('Quote Summary')
+                            ->schema([
+                                Section::make()
+                                    ->schema([
+                                        Repeater::make('items')
+                                            ->columns(4)
+                                            ->live()
+                                            ->schema([
+                                                TextInput::make('quantity')
+                                                    ->numeric()
+                                                    ->required()
+                                                    ->live()
+                                                    ->default(1),
+                                                TextInput::make('description')
+                                                    ->required()
+                                                    ->placeholder('Aerial Spraying'),
+                                                TextInput::make('unit_price')
+                                                    ->required()
+                                                    ->live()
+                                                    ->numeric()
+                                                    ->default(1000),
+                                                Placeholder::make('sum')
+                                                    ->label('Sub Total')
+                                                    ->live()
+                                                    ->content(function (Get $get) {
+                                                        return number_format($get('quantity') * $get('unit_price'));
+                                                    }),
+                                            ])
+                                            ->afterStateUpdated(function (Get $get, Set $set) {
+                                                self::updateTotals($get, $set);
+                                            })
+                                            ->addActionLabel('Add Item')
+                                            ->columnSpanFull(),
+                                    ]),
+                                Section::make()
+                                    ->schema([
+                                        TextInput::make('subtotal')
+                                            ->numeric()
+                                            ->readOnly()
+                                            ->prefix(fn(Get $get) => Currency::where('id', $get('currency_id'))->first()->abbr ?? 'CUR')
+                                            ->afterStateHydrated(function (Get $get, Set $set) {
+                                                self::updateTotals($get, $set);
+                                            }),
+                                        TextInput::make('taxes')
+                                            ->suffix('%')
+                                            ->required()
+                                            ->numeric()
+                                            ->default(16)
+                                            ->live(true)
+                                            ->afterStateUpdated(function (Get $get, Set $set) {
+                                                self::updateTotals($get, $set);
+                                            }),
+                                        TextInput::make('total')
+                                            ->numeric()
+                                            ->readOnly()
+                                            ->prefix(fn(Get $get) => Currency::where('id', $get('currency_id'))->first()->abbr ?? 'CUR'),
+                                    ]),
+                            ]),
+                        RichEditor::make('notes')
+                            ->disableToolbarButtons([
+                                'attachFiles',
+                            ])
+                    ])
+                    ->action(function(array $data, $record) {
+                        $quote = $record->quote()->create([
+                            'task' => true,
+                            'user_id' => $record->assigned_for,
+                            'vertical_id' => $record->vertical_id,
+                            'subtotal' => $data['subtotal'],
+                            'currency_id' => $data['currency_id'],
+                            'taxes' => $data['taxes'],
+                            'total' => $data['total'],
+                            'items' => $data['items'],
+                            'series' => $data['series'],
+                            'serial_number' => $serial_number = Quote::max('serial_number') + 1,
+                            'serial' => $data['series'].'-'.str_pad($serial_number, 5, '0', STR_PAD_LEFT),
+                            'notes' => $data['notes']
+                        ]);
+
+                        $recipients = User::role(Role::ADMIN)->get();
+
+                        foreach ($recipients as $recipient) {
+                            Notification::make()
+                                ->title('Quote generated')
+                                ->body(auth()->user()->name.' generated a quote for task #'.$record->id)
+                                ->icon('heroicon-o-check-badge')
+                                ->info()
+                                ->actions([
+                                    ActionsAction::make('View')
+                                        ->url(QuoteResource::getUrl('view', ['record' => $quote->id]))
+                                        ->markAsRead(),
+                                ])
+                                ->sendToDatabase($recipient);
+                        }
+                    })
             ]),
         ];
+    }
+
+    public static function updateTotals(Get $get, Set $set): void
+    {
+        $items = collect($get('items'));
+
+        $subtotal = 0;
+
+        foreach ($items as $item) {
+            $aggregate = $item['quantity'] * $item['unit_price'];
+
+            $subtotal += $aggregate;
+        }
+
+        $set('subtotal', number_format($subtotal, 2, '.', ''));
+        $set('total', number_format($subtotal + ($subtotal * ($get('taxes') / 100)), 2, '.', ''));
     }
 }
