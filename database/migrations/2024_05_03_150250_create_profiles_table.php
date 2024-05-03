ed981a2a57a0d1d2424c8b9d0d7a4068a2b76c20
njugunamwangi <njugunamwangi.n@gmail.com>
2024-05-03 15:09:21 +0300

    Profile model

---
+++ database/migrations/2024_05_03_150250_create_profiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Enums\EntityType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('logo', 2000);
+            $table->enum('entity', EntityType::values())->default(EntityType::DEFAULT);
+            $table->string('registration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
