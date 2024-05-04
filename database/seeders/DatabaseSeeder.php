e8bc1ccc422ac767abdb78cb226ab79581e19666
njugunamwangi <njugunamwangi.n@gmail.com>
2024-05-04 10:35:56 +0300

    Notes seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,7 @@ class DatabaseSeeder extends Seeder
             ServiceSeeder::class,
             TaskSeeder::class,
             AccountSeeder::class,
+            NotesSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/NotesSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Note;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class NotesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Note::create([
+            'quotes' => '<ol><li>All Prices are in Kenya shillings</li><li>50% deposit is mandatory before commence of the works</li><li>All required permits must be acquired before commence of works</li></ol>',
+            'invoices' => '<ol><li>All Prices are in Kenya shillings</li><li>50% deposit is mandatory before commence of the works</li><li>All required permits must be acquired before commence of works</li></ol>',
+        ]);
+    }
+}
