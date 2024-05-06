ef76664908370a238ffe2c8ea47a8b730ea87555
njugunamwangi <njugunamwangi.n@gmail.com>
2024-05-06 21:11:52 +0300

    Totals in base currency

--- app/Filament/Resources/InvoiceResource/Widgets/InvoiceStatsOverview.php
+++ app/Filament/Resources/InvoiceResource/Widgets/InvoiceStatsOverview.php
@@ -4,8 +4,14 @@ namespace App\Filament\Resources\InvoiceResource\Widgets;
 
 use App\Enums\InvoiceStatus;
 use App\Models\Invoice;
+use App\Models\Profile;
+use Brick\Math\RoundingMode;
+use Brick\Money\CurrencyConverter;
+use Brick\Money\ExchangeRateProvider\ConfigurableProvider;
+use Brick\Money\Money;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Facades\Http;
 
 class InvoiceStatsOverview extends BaseWidget
 {
@@ -16,7 +22,7 @@ class InvoiceStatsOverview extends BaseWidget
                 ->color('primary')
                 ->description('Number of invoices generated')
                 ->descriptionIcon('heroicon-o-calculator'),
-            Stat::make('Invoices Total amount', 0)
+            Stat::make('Invoices Total amount', self::totals())
                 ->color('warning')
                 ->description('Exclusive of taxes')
                 ->descriptionIcon('heroicon-o-banknotes'),
@@ -25,5 +31,31 @@ class InvoiceStatsOverview extends BaseWidget
                 ->description('Number of invoices paid')
                 ->descriptionIcon('heroicon-o-check-badge'),
         ];
+    }
+
+    public function totals() {
+        $api = Profile::find(1)->exchange_rate_api;
+
+        $baseCurrency = Profile::find(1)->currency->abbr;
+
+        $invoices = Invoice::all();
+
+        $sum = Money::of(0, $baseCurrency);
+
+        $exchangeRateProvider = new ConfigurableProvider();
+
+        foreach($invoices as $invoice) {
+            $rates = Http::get('https://v6.exchangerate-api.com/v6/'. $api .'/latest/'.$invoice->currency->abbr)->json()['conversion_rates'];
+
+            $exchangeRateProvider->setExchangeRate($invoice->currency->abbr, $baseCurrency, $rates[$baseCurrency]);
+
+            $converter = new CurrencyConverter($exchangeRateProvider);
+
+            $amount = $converter->convert( moneyContainer: $invoice->subtotal, currency: $baseCurrency, roundingMode: RoundingMode::UP);
+
+            $sum = $sum->plus($amount);
+        }
+
+        return $sum;
     }
 }
--- app/Filament/Resources/QuoteResource/Widgets/QuoteOverviewStats.php
+++ app/Filament/Resources/QuoteResource/Widgets/QuoteOverviewStats.php
@@ -2,9 +2,16 @@
 
 namespace App\Filament\Resources\QuoteResource\Widgets;
 
+use App\Models\Profile;
 use App\Models\Quote;
+use Brick\Math\RoundingMode;
+use Brick\Money\CurrencyConverter;
+use Brick\Money\ExchangeRateProvider\ConfigurableProvider;
+use Brick\Money\Money;
+use Brick\Money\MoneyBag;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Facades\Http;
 
 class QuoteOverviewStats extends BaseWidget
 {
@@ -20,10 +27,36 @@ class QuoteOverviewStats extends BaseWidget
                 ->color('success')
                 ->description('Number of quotes generated')
                 ->descriptionIcon('heroicon-o-calculator'),
-            Stat::make('Quotes Total amount', 0)
+            Stat::make('Quotes Total amount', self::totals())
                 ->color('primary')
                 ->description('Exclusive of taxes')
                 ->descriptionIcon('heroicon-o-banknotes'),
         ];
+    }
+
+    public function totals() {
+        $api = Profile::find(1)->exchange_rate_api;
+
+        $baseCurrency = Profile::find(1)->currency->abbr;
+
+        $quotes = Quote::all();
+
+        $sum = Money::of(0, $baseCurrency);
+
+        $exchangeRateProvider = new ConfigurableProvider();
+
+        foreach($quotes as $quote) {
+            $rates = Http::get('https://v6.exchangerate-api.com/v6/'. $api .'/latest/'.$quote->currency->abbr)->json()['conversion_rates'];
+
+            $exchangeRateProvider->setExchangeRate($quote->currency->abbr, $baseCurrency, $rates[$baseCurrency]);
+
+            $converter = new CurrencyConverter($exchangeRateProvider);
+
+            $amount = $converter->convert( moneyContainer: $quote->subtotal, currency: $baseCurrency, roundingMode: RoundingMode::UP);
+
+            $sum = $sum->plus($amount);
+        }
+
+        return $sum;
     }
 }
