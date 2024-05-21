d8595f3d07d52b8b8d8559fddb55605a4c60f6b2
njugunamwangi <njugunamwangi.n@gmail.com>
2024-05-21 17:45:17 +0300

    Filament activity logs

---
+++ lang/vendor/filament-logger/pt_BR/filament-logger.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => 'Configurações',
+    'nav.log.label' => 'Registro de atividade',
+    'nav.log.icon' => 'heroicon-o-clipboard-document-list',
+    'resource.label.log' => 'Registro de atividade',
+    'resource.label.logs' => 'Registros de atividade',
+    'resource.label.user' => 'Usuário',
+    'resource.label.subject' => 'Assunto',
+    'resource.label.subject_type' => 'Tipo de assunto',
+    'resource.label.description' => 'Descrição',
+    'resource.label.type' => 'Tipo',
+    'resource.label.event' => 'Evento',
+    'resource.label.logged_at' => 'Registrado em',
+    'resource.label.properties' => 'Propriedades',
+    'resource.label.old' => 'Antigo',
+    'resource.label.new' => 'Novo',
+    'resource.label.old_value' => 'Valor antigo',
+    'resource.label.new_value' => 'Novo valor',
+    'resource.label.properties_hint' => 'Pode ser uma chave ou valor',
+    'resource.label.old_attributes' => 'Atributo ou Valor Antigo: ',
+    'resource.label.new_attributes' => 'Atributo ou Valor Novo: ',
+];
