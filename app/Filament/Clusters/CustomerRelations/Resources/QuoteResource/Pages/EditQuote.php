634bce63c8665049689824e62a0cdafe279a584f
njugunamwangi <njugunamwangi.n@gmail.com>
2024-08-23 15:39:10 +0300

    Edit proforma

--- app/Filament/Clusters/CustomerRelations/Resources/QuoteResource/Pages/EditQuote.php
+++ app/Filament/Clusters/CustomerRelations/Resources/QuoteResource/Pages/EditQuote.php
@@ -3,8 +3,15 @@
 namespace App\Filament\Clusters\CustomerRelations\Resources\QuoteResource\Pages;
 
 use App\Filament\Clusters\CustomerRelations\Resources\QuoteResource;
+use App\Mail\SendQuote;
+use App\Models\Role;
+use App\Models\User;
 use Filament\Actions;
+use Filament\Notifications\Actions\Action;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
 
 class EditQuote extends EditRecord
 {
@@ -23,5 +30,38 @@ class EditQuote extends EditRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('view', ['record' => $this->getRecord()]);
+    }
+
+    protected function afterCreate(): void
+    {
+        $quote = $this->getRecord();
+
+        if ($quote->mail) {
+
+            $quote->savePdf();
+
+            Mail::to($quote->user->email)->send(new SendQuote($quote));
+
+            $recipients = User::role(Role::ADMIN)->get();
+
+            foreach ($recipients as $recipient) {
+                Notification::make()
+                    ->warning()
+                    ->icon('heroicon-o-bolt')
+                    ->title('Quote mailed')
+                    ->body('Quote mailed to '.$quote->user->name)
+                    ->actions([
+                        Action::make('view')
+                            ->markAsRead()
+                            ->url(QuoteResource::getUrl('view', ['record' => $quote->id]))
+                            ->color('warning'),
+                    ])
+                    ->sendToDatabase($recipient);
+            }
+
+            $name = 'invoice_'.$quote->series->name.'_'.str_pad($quote->serial_number, 5, '0', STR_PAD_LEFT).'.pdf';
+
+            Storage::disk('quotes')->delete($name);
+        }
     }
 }
